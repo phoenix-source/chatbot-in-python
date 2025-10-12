c13d667fb8ab1f3fe0dd7acd851d10193d96bb4d
Niraj Nillawar <149431848+NirajDN@users.noreply.github.com>
2025-10-12 20:43:00 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,113 @@
+// REPL===READ EVALUVATE PRINT LOOP
+// We can perform node js coding in terminal
+
+const { use } = require("react");
+
+// console.log("5"+1);
+
+// function fruit(item){
+//     console.log("fruit is "+ item);
+// }
+
+// fruit("apple");
+// fruit("banana");
+// fruit("grape");
+
+
+// function sum(a,b){
+//     console.log("sum is "+ (a+b));
+// }
+// sum(5,10);
+// sum(20,30);
+
+
+// var fs = require('fs');
+// fs.writeFileSync('output.txt','Hello My name is Niraj');
+
+
+// var a=10;
+// let b=20;
+// const c=55;
+// a=9;
+// b=5;
+// console.log("sum is "+(a+b+c));
+
+
+
+
+
+//Conditional Statements
+// if(a>b){
+//     console.log("a is greater");
+// }
+// else if(a==b){
+//     console.log("a and b are equal");
+// }
+// else{
+//     console.log("b is greater");
+// }
+
+
+
+
+
+//Loops
+// for(let i =0;i<=12;i++){
+//     if(i==5){
+//     console.log("value of i is "+i);
+// }
+// }
+// var d= 10;
+// while(d>5){
+//     console.log("value of d is "+d);
+//     d--;
+// }
+
+
+
+//Arrays
+// var users=["Niraj","Adarsh","Harsh"];
+// console.log(users[0]);
+// users.push("Parth");
+// console.log(users[3]);
+// for(var a=0;a<users.length;a++){
+//     console.log("user is "+users[a]);
+// }
+
+// var kk=22;
+// if(kk%2==0){
+//     console.log("even number");
+// }
+// else{
+//     console.log("odd number");
+// }
+
+//factorial of number 
+
+
+// var n=5;
+// for(var i=n-1;i>=1;i--){
+//     n=n*i;
+// }
+// console.log("fact is "+n);
+
+
+//Table of number
+// var tab=5;
+// for(var y=1;y<=10;y++){
+//     console.log("5*"+y+"="+(5*y));
+// }
+
+
+
+// use of object
+var stud={
+    name:"Niraj",
+    age:21,
+    city:"Mumbai"
+}
+console.log(stud);
+console.log(stud.name);
+console.log(stud.age);
+console.log(stud.city);
+
